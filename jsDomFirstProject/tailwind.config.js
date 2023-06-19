e3464e75144da4f63b8fcb19a52dfff2f16d08a3
Ak-ko <waterbubbles.ww6615@gmail.com>
2023-06-19 14:37:27 +0630

    done-merging-and-pushed

---
+++ jsDomFirstProject/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.html'
+  ],
+  theme: {
+    extend: {
+      colors: [
+
+      ],
+    },
+  },
+  plugins: [],
+}
+
